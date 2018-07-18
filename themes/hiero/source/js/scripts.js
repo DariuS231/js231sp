6c862d3fd00b27fad7ae838005f86d834c95610c
Dario Alvarez <Dario.Alvarez@contentandcode.com>
2018-07-18 15:54:44 +0100

    Cleaning up

--- themes/hiero/source/js/scripts.js
+++ themes/hiero/source/js/scripts.js
@@ -15,15 +15,15 @@
     }, searchAnimDuration);
   };
 
-  var s = [
-        '<div style="display: none;">',
-          '<script src="https://s11.cnzz.com/z_stat.php?id=1260716016&web_id=1260716016" language="JavaScript"></script>',
-        '</div>'
-      ].join('');
+  // var s = [
+  //       '<div style="display: none;">',
+  //         '<script src="https://s11.cnzz.com/z_stat.php?id=1260716016&web_id=1260716016" language="JavaScript"></script>',
+  //       '</div>'
+  //     ].join('');
 
-  var di = $(s);
+  // var di = $(s);
 
-  $('#container').append(di);
+  // $('#container').append(di);
 
   $('#nav-search-btn').on('click', function(){
     if (isSearchAnim) return;
